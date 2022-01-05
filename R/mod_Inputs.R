5a2449affdf24e214f4ae25c37d4dc1e9e7d368c
Etienne Rifa <etienne.rifa[at]insa-toulouse.fr>
2022-01-05 12:26:07 +0100

    PCA tab

--- R/mod_Inputs.R
+++ R/mod_Inputs.R
@@ -6,7 +6,16 @@
 #'
 #' @noRd 
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
+#' @importFrom tibble rownames_to_column 
+#' @importFrom dplyr right_join
+#' @importFrom plotly plotlyOutput
+#' @importFrom plotly renderPlotly
+#' @importFrom plotly ggplotly
+#' @importFrom plotly config
+#' @import ggplot2
+#' @import DT
+
 mod_Inputs_ui <- function(id){
   ns <- NS(id)
   tagList(
@@ -59,7 +68,38 @@ mod_Inputs_ui <- function(id){
                   ),
                   shinyBS::bsButton(inputId = ns('norm'), label = "Normalize", block = F, style = 'danger', type='action')
                 )
+              ),
+            fluidRow(
+              box(width=12,
+                title = 'Merged Table preview', status = "primary", solidHeader = TRUE, collapsible = TRUE, collapsed = FALSE,
+                DT::dataTableOutput(ns("mergedf_DT")),
+                downloadButton(outputId = ns("mergedf_download"), label = "Download merged table")
               )
+            )
+        ),
+        tabPanel("PCA",
+                 fluidRow(
+                   box(title = "Settings:", width = 6, status = "warning", solidHeader = TRUE,
+                       # uiOutput(ns("factor1")),
+                       selectInput(
+                         ns("fact1"),
+                         label = "Factor to color samples in PCA:",
+                         choices = ""
+                       ),
+                       actionButton(ns("go1"), "Plot ACP", icon = icon("play-circle"),
+                                    style="color: #fff; background-color: #3b9ef5; border-color: #1a4469")
+                   )
+                 ),
+                 fluidRow(box(width = 12, 
+                              title = 'ACP table', status = "primary", solidHeader = TRUE, collapsible = TRUE, collapsed = FALSE,
+                              DT::dataTableOutput(ns("prevacp1"))
+                              )
+                          ),
+                 fluidRow(box(width = 12, 
+                              title = 'ACP plot', status = "primary", solidHeader = TRUE, collapsible = TRUE, collapsed = FALSE,
+                              plotlyOutput(ns("acpplot"))
+                              )
+                          )
         ),
         tabPanel("Boxplots",
                  fluidRow()
@@ -80,11 +120,13 @@ mod_Inputs_server <- function(id, r = r, session = session){
     dataset1 <- reactive({
       cat(file=stderr(), 'dataset1 fun', "\n")
       if (!is.null(input$dataset1)){
-        r_values$ds1 <- read.table(input$dataset1$datapath, sep = "\t", dec = ",", header = TRUE)
+        ds1 <- read.table(input$dataset1$datapath, sep = "\t", dec = ",", header = TRUE)
+        row.names(ds1) <- ds1[,1]
+        r_values$ds1 <- ds1
       }
-      else{
-        print("no data")
-      }
+      # else{
+      #   print("no data")
+      # }
       r_values$ds1
     })
     
@@ -92,8 +134,7 @@ mod_Inputs_server <- function(id, r = r, session = session){
       cat(file=stderr(), 'metadata1 fun', "\n")
       if (!is.null(input$metadata1)){
         r_values$mt1 <- read.table(input$metadata1$datapath, sep = "\t", dec = ",", header = TRUE)
-      }
-      else{
+      }else{
         cat(file=stderr(), 'metadata1 is null', "\n")
         r_values$mt1 = NULL
       }
@@ -101,52 +142,170 @@ mod_Inputs_server <- function(id, r = r, session = session){
     })
     
     output$prevds1 <- renderPrint({
-      cat(file=stderr(), 'rendering ds1', "\n")
+      cat(file = stderr(), 'rendering ds1', "\n")
       cat('Running graphstats v0.0.0.9000\n')
-      head(dataset1()[,1:6])
+      head(dataset1()[, 1:6])
+      if (is.null(dataset1())) {
+        print("no data")
+      } else if (ncol(dataset1()) > 6) {
+        head(dataset1()[, 1:6])
+      } else{
+        head(dataset1()[, 1:ncol(dataset1())])
+      }
     })
     
     output$prevmt1 <- renderPrint({
-      cat(file=stderr(), 'rendering mt1', "\n")
-      if(is.null(metadata1())){print("no data")
-        }else if(ncol(metadata1())>6){
-        head(metadata1()[,1:6])
-      }else{
-        head(metadata1()[,1:ncol(metadata1())])
+      cat(file = stderr(), 'rendering mt1', "\n")
+      if (is.null(metadata1())) {
+        print("no data")
+      } else if (ncol(metadata1()) > 6) {
+        head(metadata1()[, 1:6])
+      } else{
+        head(metadata1()[, 1:ncol(metadata1())])
       }
       
     })
     
-    normalize <- reactive({
-      req(r_values$phyobj_final, input$norm_method)
-      FGdata <- r_values$phyobj_final
+    output$normds1 <- renderPrint({
+      cat(file = stderr(), 'rendering normds1', "\n")
+      if (is.null(metadata1())) {
+        print("no data")
+      } else if (ncol(metadata1()) > 6) {
+        head(metadata1()[, 1:6])
+      } else{
+        head(metadata1()[, 1:ncol(metadata1())])
+      }
+      
+    })
+    
+    
+    output$mergedf <- renderPrint({
+      cat(file = stderr(), 'rendering mergedf', "\n")
+      if (is.null(mergedf())) {
+        print("no data")
+      } else if (ncol(mergedf()) > 6) {
+        head(mergedf()[, 1:6])
+      } else{
+        head(mergedf()[, 1:ncol(mergedf())])
+      }
+      
+    })
+    
+    observeEvent(input$norm, {
+      cat(file=stderr(), 'button normalize', "\n")
+      normds1()
+    },ignoreNULL = TRUE, ignoreInit = TRUE)
+    
+    normds1 <- reactive({
+      req(r_values$ds1, input$norm_method)
+      ds1 <- r_values$ds1
       
       if(input$norm_method == 0){
-        FNGdata <- FGdata
+        normds1 <- ds1
       }
       
       if(input$norm_method == 1){
         normf = function(x){ x/sum(x) }
-        FNGdata <- transform_sample_counts(FGdata, normf)
+        # normds1 <- transform_sample_counts(ds1, normf)
+        normds1 <- apply(ds1[,-1], 2, normf)
       }
       
       if(input$norm_method == 2){
-        clr = function(x){log(x+1) - rowMeans(log(x+1))}
-        otable <- otu_table(FGdata)
-        otableCLR <- clr(otable)
-        FNGdata <- FGdata; otu_table(FNGdata) <- otableCLR
+        clr = function(x){log(x+1) - rowMeans(log(x+1), na.rm = TRUE)}
+        normds1 <- clr(ds1[,-1])
       }
-      
-      #VST deseq2
-      if(input$norm_method == 3){
-        withProgress({
-          otable <- FGdata@otu_table@.Data+1
-          otableVST <- DESeq2::varianceStabilizingTransformation(otable, fitType='local')
-          FNGdata <- FGdata; FNGdata@otu_table@.Data <- otableVST
-        },message = "VST normalization, please wait...")
-      }
-      r_values$phyobj_norm <- FNGdata
+
+      r_values$normds1 <- normds1
       # print(r_values$phyobj_norm)
+    })
+    
+    mergedf <- reactive({
+      req(r_values$normds1, r_values$mt1)
+      r_values$tabF = as.data.frame(t(normds1())) %>% 
+        tibble::rownames_to_column(var = "sample.id") %>% 
+        dplyr::right_join(x = metadata1(), by = "sample.id")
+    })
+    
+    output$mergedf_download <- downloadHandler(
+      filename = "merged_table.csv",
+      content = function(file) {
+        req(r_values$tabF)
+        write.table(r_values$tabF, file, sep="\t", row.names=FALSE)
+      }
+    )
+    
+    
+    rowCallback <- c(
+      "function(row, data){",
+      "  for(var i=0; i<data.length; i++){",
+      "    if(data[i] === null){",
+      "      $('td:eq('+i+')', row).html('NA')",
+      "        .css({'color': 'rgb(151,151,151)', 'font-style': 'italic'});",
+      "    }",
+      "  }",
+      "}"
+    )
+    
+    output$mergedf_DT <- DT::renderDataTable({
+      mergedf()
+    }, filter="top",options = list(pageLength = 5, scrollX = TRUE, rowCallback = DT::JS(rowCallback)), server=TRUE)
+    
+    
+    ### ACP tab
+    
+    # output$factor1 = renderUI({
+    #   req(metadata1())
+    #   selectInput(
+    #     ns("fact1"),
+    #     label = "Select factor to test: ",
+    #     choices = names(metadata1())
+    #   )
+    # })
+    
+    observe({
+      req(metadata1())
+      updateSelectInput(session, "fact1",
+                        choices = names(metadata1()),
+                        selected = names(metadata1())[1])
+    })
+    
+      
+    acptab <- reactive({
+      req(r_values$normds1, r_values$mt1)
+      # print(head(normds1()))
+      # print(str(normds1()))
+      acp1 = stats::prcomp(na.omit(t(normds1()[,-1])), scale. = TRUE)
+      
+      r_values$acp1 <- acp1
+      
+      acptab= as.data.frame(acp1$x) %>% tibble::rownames_to_column(var = "sample.id") %>% 
+        dplyr::inner_join(x = metadata1(), by = "sample.id")
+      acptab
+
+    })
+    
+    output$prevacp1 <- DT::renderDataTable({
+      cat(file=stderr(), 'ACP table', "\n")
+      acptab()
+    }, filter="top",options = list(pageLength = 5, scrollX = TRUE, rowCallback = DT::JS(rowCallback)), server=TRUE) 
+    
+    acpplot <- eventReactive(input$go1, {
+      req(input$fact1, acptab())
+    # acpplot <- reactive({
+      print("coucou")
+      print(input$fact1)
+      
+      cat(file=stderr(), 'ACP plot', "\n")
+      print("coucou2")
+      p = ggplot(acptab(), aes(x = PC1, y =
+                               PC2, color = input$fact1, sampleID = sample.id)) + 
+        geom_point() + stat_ellipse(aes(x = PC1, y = PC2, color = input$fact1), inherit.aes = FALSE)  # pb type aes_string...
+      ggplotly(p, tooltip=c("x", "y", "sampleID"))
+    })
+      
+    output$acpplot <- renderPlotly({
+      req(acpplot())
+      acpplot() %>% config(toImageButtonOptions = list(format = "svg"))
     })
  
   })
