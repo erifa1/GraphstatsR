4f727be098064da5f30cc2e0de412682602149c1
Etienne Rifa <etienne.rifa@inra.fr>
2023-11-16 14:50:17 +0100

    v1.9.0 add r base graphics, faster rendering

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,11 @@
 #' @param x a dataframe
 #' 
 
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
 replace_mu <- function(x){
   for(i in 1:ncol(x)){
     if(is.factor(x[,i])){
