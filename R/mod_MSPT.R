9608a8246ddd912d869931f0d950a5f6a75c01df
Etienne Rifa <etienne.rifa[at]insa-toulouse.fr>
2024-09-30 15:48:15 +0200

    first widgets

--- R/mod_MSPT.R
+++ R/mod_MSPT.R
@@ -10,7 +10,14 @@
 mod_MSPT_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    box(title = "Settings:", width = 7, status = "warning", solidHeader = TRUE,
+      fileInput(ns("file"), "Choose a file", accept = c(".csv", ".tsv", ".txt", ".xlsx", ".xls")),
+      numericInput(ns("p1"), "Value of p:", 0.513, min = 0, max = NA)
+    ),
+    box(title = "Preview:", width = 12, status = "warning", solidHeader = TRUE,
+    tableOutput(ns("contents")),
+    plotOutput(ns("plot1"))
+    )
   )
 }
     
@@ -20,7 +27,26 @@ mod_MSPT_ui <- function(id){
 mod_MSPT_server <- function(id, session=session, r=r){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+    r_values <- reactiveValues(res = NULL)
+
+
+
+    output$contents <- renderTable({
+      # head(rio::import(input$file$datapath))
+
+      res <- r_values$res <- MSPT_fun(input$file$datapath, p = input$p1, outpath = "./data_test/dev_mspt/MSPT_out3/")
+      head(res$Table)
+
+    })
+
+
+    output$plot1 <- renderPlot({
+
+      res <- r_values$res
+      res$figures[[1]]
+
+    })
+
   })
 }
     
